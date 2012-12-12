8dca8c428ea2ca851573999d6365b99a4229cff6
Tony Thomas <thoma127@umn.edu>
2012-12-12 11:52:13 -0600

    Added SASS file and JS updates. Sine wave working, but not quite as desired.

--- js/audio.js
+++ js/audio.js
@@ -1,6 +1,41 @@
-var context = new webkitAudioContext(),
-    oscillator = context.createOscillator;
-oscillator.type = 0;
-oscillator.frequency.value = 500;
-oscillator.connect(context.destination); // Connect to speakers
-oscillator.noteOn(0); // Autoplay
+if('webkitAudioContext' in window)
+{
+	var context = new webkitAudioContext();
+	var oscillator_sine = context.createOscillator();
+	var default_frequency = 261.33;
+	oscillator_sine.type = 0; // 0 = sine wave
+	oscillator_sine.frequency.value = default_frequency;
+	oscillator_sine.connect(context.destination); // Connect to speakers
+}
+else
+{
+	$('body').append('<p>webkitAudioAPI not supported.');
+}
+
+$(function()
+{
+	$('button').on('click', function()
+	{
+		if ($(this).data('control') == "stop")
+		{
+			oscillator_sine.noteOff(0);
+		}
+
+		if ($(this).data('control') == "start")
+		{
+		//	oscillator_sine.noteOn();
+			for (var i = 0; i < 8; i++)
+			{
+			    var rest = i / 16;
+			    oscillator_sine.noteOn(context.currentTime + rest);
+			}
+		}
+	});
+
+	$('li').on('mousedown', function()
+	{
+		frequency = $(this).data('frequency');
+		oscillator_sine.frequency.value = frequency;
+		oscillator_sine.noteOn(0);
+	});
+});
